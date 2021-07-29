d98220cc78c352e91d18d1c940b0921946ebe035
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-29 14:11:12 -0300

    ativa navegacao pelos botoes

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -20,15 +21,17 @@ export default function UserList(){
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários </Heading>
 
-                        <Button 
-                         as="a" 
-                         size="sm" 
-                         fontSize="sm" 
-                         colorScheme="pink"
-                         leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                         >
-                            Criar novo usuário
-                        </Button>
+                       <Link href="/users/create" passHref>
+                            <Button 
+                              as="a" 
+                              size="sm" 
+                              fontSize="sm" 
+                              colorScheme="pink"
+                              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                              >
+                                Criar novo usuário
+                            </Button>
+                       </Link>
                     </Flex>
                 
                     <Table colorScheme="whiteAlpha">
