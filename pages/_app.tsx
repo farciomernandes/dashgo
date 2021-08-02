a7537dba80267efc8f81e5ff55fff030ee252025
Marcio Fernandes <farciomernandes@gmail.com>
2021-08-02 09:35:32 -0300

    cria api fake com miraclejs e gera dados fakes automaticos com a lib faker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,14 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  if(process.env.NODE_ENV === 'development'){
+    makeServer();
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <SidebarDrawerProvider>
---
+++ services/mirage/index.ts
@@ -0,0 +1,50 @@
+import {  createServer, Factory, Model } from 'miragejs';
+import faker from 'faker'; //Lib que cria dados fakes
+
+type User = {
+    name: string;
+    email: string;
+    create_at: string;
+}
+
+export function makeServer(){
+    const server = createServer({
+        models: { //banco de dados ficticio
+            user: Model.extend<Partial<User>>({ //Partial = Talvez tenha dados que nao possuam tudo o que foi informado no User
+
+            })
+        },
+
+        factories: { //criação dos dados iniciais da apifake
+            user: Factory.extend({
+                name(indice: number){
+                    return `User ${indice +1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase();
+                },
+                createdAt(){
+                    return faker.date.recent(10); //Cria datas random a partir de 10 dias atrás ate a data atual
+                }
+            })
+        },
+
+        seeds(server){ //Dados iniciais na api fake
+            server.createList('user', 200); //nome da factories que deve criar e quantidade
+        },
+
+        routes(){
+
+            this.namespace = 'api'; //avisa que para chamar o '/users' é preciso por '/api' antes
+            this.timing = 750; //avisa que a api deve aguardar 0.75s para resolver
+
+            this.get("/users"); //Shorthands => O miragejs entende pelo nome da rota que deve retornar todos os itens da tabela com o nome da rota
+            this.post("/users"); //Shorthands => O miragejs entende pelo nome da rota que deve adicionar um item na tabela com o nome da rota
+            
+            this.namespace = '' ;
+            this.passthrough();
+        }
+    })
+
+    return server;
+}
